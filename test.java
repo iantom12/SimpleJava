ea944879c7847f29d7fefdc08e55763367ff495d
iantom12 <109991370+iantom12@users.noreply.github.com>
2022-12-14 12:00:22 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,9 @@
-System.out.println("Hello World");
+public class TestWorld {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("The World has been tested, Successfully");
+    }
+
+}
