8f6d97a06d0e956abc4c9cbb70316f98be1e9d91
iantom12 <109991370+iantom12@users.noreply.github.com>
2022-12-14 12:01:48 -0500

    Update and rename test.java to javadeploy.java

--- javadeploy.java
+++ javadeploy.java
@@ -3,7 +3,7 @@ public class TestWorld {
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("The World has been tested, Successfully");
+        System.out.println("The World has been deployed successfully);
     }
 
 }
