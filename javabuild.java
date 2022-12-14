831a156248e872dc5f53fddfcf0f8d70d94c7123
iantom12 <109991370+iantom12@users.noreply.github.com>
2022-12-14 12:18:29 -0500

    Update javabuild.java

--- javabuild.java
+++ javabuild.java
@@ -1,10 +1,10 @@
 
-public class HelloWorld {
+public class BuildWorld {
 
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("World has been successfully Built");
     }
 
 }
