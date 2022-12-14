64c10592ab069195d61540754c9b88bc56289e07
iantom12 <109991370+iantom12@users.noreply.github.com>
2022-12-14 12:03:28 -0500

    Create javaunit.java

---
+++ javaunit.java
@@ -0,0 +1,9 @@
+public class WorldUnitTest {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("World Unit has been tested and successful");
+    }
+
+}
